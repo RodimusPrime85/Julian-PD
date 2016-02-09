9b94be80c9688b7d85847d9968338889e28e3bc1
RodimusPrime85 <strayhron.design@gmail.com>
2016-02-08 21:50:51 -0500

    hot

--- js/common.js
+++ js/common.js
@@ -1,1 +1,33 @@
+var imgGal = $(function( img, e ) {
 
+      var $overlay = $('<div id="overlay"></div>'),
+          $image   = $("<img>"),
+          $caption = $('<p class="caption"></p>');
+
+
+      $overlay.append($image);
+
+    
+      $("body").append($overlay);
+
+
+      var imgLinks = $('#imageGallery a');
+
+      imgLinks.click(function( e ) {
+        e.preventDefault();
+        var imgLoc = $(this).attr('href');
+        $image.attr('src', imgLoc);
+        //add caption to overlay
+        $caption.insertAfter($image);
+        $image.addClass('borderedImg');
+        $overlay.slideDown();
+
+      var captionText = $(this).children('img').attr('alt');
+        $caption.text(captionText);
+
+      });
+
+      $overlay.click(function() {
+        $overlay.slideUp();
+      });
+});
